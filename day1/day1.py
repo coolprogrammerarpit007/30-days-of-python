51cf4a7f69595f3ee51db7926e6e3de9cd624080
microprixs <arpit.mishra@microprixs.in>
2025-05-09 12:11:03 +0530

    completed day-1 and day-2 of challenge

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+# 30 days Python challenge
+
+# Arithmetic Operations
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3**2)
+print(3//2)
+print(3%2)
+
+
+# datatypes in python
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('Asabenah'))
+print(type([1,2,3]))
+print(type({'name':'Asabenah'}))
+print(type({9.8,3.7,4.5}))
+print(type((9.8,3.7,85.4)))
+print(type(3==3))
+print(type(3 >= 3))
