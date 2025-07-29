b1909d89b03d2a6bc81d605ffe30f93a41fb8680
microprixs <arpit.mishra@microprixs.in>
2025-07-29 19:03:31 +0530

    Day 1 challenges

---
+++ day_1/day1_exercise.py
@@ -0,0 +1,44 @@
+# Exercise Level 1
+
+# print(3+4)
+# print(3-4)
+# print(3*4)
+# print(3/4)
+# print(3//4)
+# print(3%4)
+# print(5%6)
+# print(3**4)
+
+
+# Exercise Level 1
+
+# print("Arpit")
+# print("Mishra")
+# print("India")
+# print("I am enjoying 30 days of python")
+
+# print(type(10))
+# print(type(9.8))
+# print(type(3.14))
+# print(type(4 - 4j))
+# print(type(['Asabenah','Python','Finland']))
+# print(type('Arpit'))
+# print(type('India'))
+# print(type('Mishra'))
+
+
+#  Exercise Level 3
+
+# print(25)
+# print(3.78)
+# print(7 +  5j)
+# print("Arpit Mishra")
+# print(True)
+# print(["Arpit","Rajesh",'"Divyansh',"Manish"])
+# print(("Earth","Mars","Venus","Saturn","Jupiter"))
+# print({78,28,95,"Novak"})
+# print({
+#     "name":"Arpit Mishra",
+#     "age":25,
+#     "is_married":True,
+# })
