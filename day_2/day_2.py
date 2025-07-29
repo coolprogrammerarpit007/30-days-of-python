4623226c063894ccbbddef6bf258c0621cd5b32d
microprixs <arpit.mishra@microprixs.in>
2025-07-29 19:57:17 +0530

    Completed day 2 exercises

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -17,3 +17,49 @@ friend_1,friend_2,friend_3 = 'Ramesh','Manish','Jayveer'
 
 # print(full_name)
 # print(friend_1,friend_2,friend_3)
+
+
+#  Exercise Level 2
+
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+# print(type(friend_1))
+# print(type(friend_2))
+# print(type(friend_3))
+
+# print(len(first_name))
+# print(len(first_name) , len(last_name))
+
+
+# num_one,num_two = 5 , 4
+# total = num_one + num_two
+# diff = num_one - num_two
+# product = num_one * num_two
+# division = num_one / num_two
+# remainder = num_two % num_one
+# exp = num_one ** num_two
+# floor_division = num_one // num_two
+# # print(total,diff,product,division,remainder,exp,floor_division)
+
+
+# PI = 3.14
+# # circle_radius = 30
+# # # circle_radius = float(input("Enter circle radius: "))
+# # area_of_circle = PI * circle_radius * circle_radius
+# # circum_of_circle = 2 * PI * circle_radius
+# # print(area_of_circle,circum_of_circle)
+
+
+# first_name = input("Enter first Name: ")
+# last_name = input("Enter last name: ")
+# country = input("Enter Country: ")
+# age = input("Enter your age: ")
+# print(f"My name is {first_name} {last_name}, I am {age} years old from {country}")
