1a9c1cd2ab29672aecbec5f5dad196708ba37bb4
microprixs <arpit.mishra@microprixs.in>
2025-07-29 19:39:36 +0530

    completed level 1 of day 2 exercise

---
+++ day_2/day_2.py
@@ -0,0 +1,19 @@
+#  Exercise Level 1
+
+# Day 2: 30 days of python programming
+
+first_name = 'Arpit'
+last_name = 'Mishra'
+full_name = first_name + ' ' + last_name
+country = 'India'
+city = 'Jaipur'
+age = 25
+year = 2000
+is_married = False
+is_true = True
+is_light_on = True
+friend_1,friend_2,friend_3 = 'Ramesh','Manish','Jayveer'
+
+
+# print(full_name)
+# print(friend_1,friend_2,friend_3)
